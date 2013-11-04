3983bf0075491589b0096054a0e31b788768af37
Nicholas Flintham <nick@flinny.org>
2013-11-04 16:43:17 +0000

    media-caf: Add MEDIA_VARIANT build barrier
    Change-Id: I199bdd867218079d6e3b3e5ae1453e1b74e92257

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm7x30 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
 
 include $(call all-subdir-makefiles)
 
+endif
 endif
